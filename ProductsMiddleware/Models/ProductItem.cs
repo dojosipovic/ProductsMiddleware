8a202ca372853063b788e08b9dcbcd0523986713
Dominik Josipović <djosipovi21@student.foi.hr>
2024-06-17 20:53:00 +0200

    Added class for product item in list

---
+++ ProductsMiddleware/Models/ProductItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductsMiddleware.Models
+{
+    public class ProductItem
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public float price { get; set; }
+        public string thumbnail { get; set; }
+    }
+}
